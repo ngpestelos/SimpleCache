ff75f5a06c47a51f07dfadeaf92bd9bc2045a7e3
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-12-31 23:51:16 +0800

    simple cache (single OTP server)

--- src/simple_cache_app.erl
+++ src/simple_cache_app.erl
@@ -10,7 +10,13 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    simple_cache_sup:start_link().
+  simple_cache_store:init(),
+  case simple_cache_sup:start_link() of
+    {ok, Pid} ->
+      {ok, Pid};
+    Other ->
+      {error, Other}
+  end.
 
 stop(_State) ->
     ok.
